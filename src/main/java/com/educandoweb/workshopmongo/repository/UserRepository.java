9bf791bbd8988ed1ccd61a4c4d8a7c5e8ac3f70c
emersonney <emerson.farias@gmail.com>
2020-07-30 15:17:15 -0300

    Obtendo um usuário por ID

--- src/main/java/com/educandoweb/workshopmongo/repository/UserRepository.java
+++ src/main/java/com/educandoweb/workshopmongo/repository/UserRepository.java
@@ -8,6 +8,4 @@ import com.educandoweb.workshopmongo.domain.User;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
-	
-	
 }
--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,15 +23,15 @@ public class UserResource {
 	
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll(){
-		/*
-		 * User emerson = new User("1", "Emerson Teste", "emersonteste@gmail.com"); User
-		 * maria = new User("2","Maria Silva","maria@gmail.com"); User alex = new
-		 * User("3","Alex Green","alex@gmail.com"); List<User> list = new ArrayList<>();
-		 * list.addAll(Arrays.asList(emerson, maria, alex));
-		 */
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); 
 		return ResponseEntity.ok().body(listDto);
 	}
+
+	@GetMapping(value="/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}	
 	
 }
