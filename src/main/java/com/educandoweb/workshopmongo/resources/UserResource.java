a8dc16d1704c09f409b04f0a8684dc83eb3d2fe5
emersonney <emerson.farias@gmail.com>
2020-07-31 15:13:12 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -51,4 +51,10 @@ public class UserResource {
 		return ResponseEntity.created(uri).build();
 	}	
 	
+	@GetMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id){
+		service.delete(id);;
+		return ResponseEntity.noContent().build();
+	}
+	
 }
